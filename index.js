5334f4e2a91d68ddf33936532e9c16ea8dca3748
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-07-20 10:01:11 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+    
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+  }
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  countDown(5); // This will count down from 5 to 0 
+  
+  
+  
+ 
